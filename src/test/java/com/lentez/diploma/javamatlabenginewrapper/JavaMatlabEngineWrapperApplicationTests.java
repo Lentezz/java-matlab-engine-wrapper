17b9566dcb4106eb4d55a04aefd352b8c7c98797
Yaroslav_Holovan <yaroslav.golovan@gmail.com>
2024-10-11 15:01:03 +0300

    Initial commit

---
+++ src/test/java/com/lentez/diploma/javamatlabenginewrapper/JavaMatlabEngineWrapperApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lentez.diploma.javamatlabenginewrapper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaMatlabEngineWrapperApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
